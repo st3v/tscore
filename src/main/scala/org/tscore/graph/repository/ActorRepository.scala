1f36913c38558a679b0f9929b966714fdcc0150c
st3v <stev@kiva.org>
2012-10-07 14:21:06 -0700

    Removing obsolete package import

--- src/main/scala/org/tscore/graph/repository/ActorRepository.scala
+++ src/main/scala/org/tscore/graph/repository/ActorRepository.scala
@@ -2,6 +2,5 @@ package org.tscore.graph.repository
 
 import org.springframework.data.neo4j.repository.GraphRepository
 import org.tscore.graph.model.Actor
-import org.tscore.graph.util.ApplicationContext
 
 trait ActorRepository extends GraphRepository[Actor]
